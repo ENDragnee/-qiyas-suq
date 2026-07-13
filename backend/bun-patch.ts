93a78b6ff159c0e379355e18239ea2ec3436de5b
ENDragnee <mesfinmastwal@gmail.com>
2026-07-13 21:29:42 +0300

    chore: remove the bun patch

--- backend/bun-patch.ts
+++
@@ -1,9 +0,0 @@
-import v8 from "node:v8";
-
-if (!v8.startupSnapshot) {
-  Object.defineProperty(v8, "startupSnapshot", {
-    value: { isBuildingSnapshot: () => false },
-    writable: true,
-    configurable: true,
-  });
-}
